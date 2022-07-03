2afc9dc5ec456cb314e4f53923cd75a172d430f6
Kostya Lapatin <klrotex11@gmail.com>
2022-07-03 16:34:29 +0300

    hw-03

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -13,12 +13,23 @@ function Affair(props: AffairPropsType) {
     const deleteCallback = () => {
         props.deleteAffairCallback(props.affair._id)
     }// need to fix
-
+    let priorityClass = ''
+    if (props.priorityClass === 'high') {
+        priorityClass = classes.high
+    }
+    if (props.priorityClass === 'middle') {
+        priorityClass = classes.middle
+    }
+    if (props.priorityClass === 'low') {
+        priorityClass = classes.low
+    }
     return (
         <div className={classes.affairWrapper}>
-            <button onClick={deleteCallback} className={classes.button}>X</button>
+            <div>
+                <button onClick={deleteCallback} className={classes.button}>X</button>
+            </div>
             <div className={classes.name}>{props.affair.name}</div>
-            <div className={classes.priority}>{props.affair.priority}</div>
+            <div className={priorityClass}>{props.affair.priority}</div>
         </div>
     )
 }
--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,26 +1,32 @@
-import React from 'react'
+import React, {ChangeEvent,KeyboardEvent} from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    name: string // need to fix any
+    setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void // need to fix any
+    addUser: () => void // need to fix any
+    error: string // need to fix any
+    totalUsers: number // need to fix any
+    onEnter: (e:KeyboardEvent<HTMLInputElement>) => void
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
+    {name, setNameCallback, addUser, error, totalUsers,onEnter} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
-
+    const inputClass = error ? s.error : s.input// need to fix with (?:)
+    const errorClass = error ? s.errorText : ''
     return (
         <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
+            <input
+                value={name}
+                onChange={setNameCallback}
+                className={inputClass}
+                onKeyDown={onEnter}
+            />
             <button onClick={addUser}>add</button>
             <span>{totalUsers}</span>
+            <div className={errorClass}>{error}</div>
         </div>
     )
 }
